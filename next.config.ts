bf6a516b83b007b9511c6d258c46c35df138be96
Braian Silva <braiannogueirasilva@gmail.com>
2026-06-18 08:54:42 -0300

    Update Next Config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  distDir:"dist",
+  output:"export",
   reactStrictMode: true,
+  images: {unoptimized:true}
 };
 
 export default nextConfig;
