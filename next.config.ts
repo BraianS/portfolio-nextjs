ec0a6295fd2401b7a5e043118578bba0dc5b3826
Braian Silva <braiannogueirasilva@gmail.com>
2026-06-18 09:22:16 -0300

    Add basePath to NextConfig

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath:"/portfolio-nextjs",
   distDir:"dist",
   output:"export",
   reactStrictMode: true,
