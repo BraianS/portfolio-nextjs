873e22c8c709bb3e71d5491c7dbb345e135a669d
Braian Silva <braiannogueirasilva@gmail.com>
2026-06-19 08:14:51 -0300

    Add Title and meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <meta name="descricao" content="Portfolio criando com NextJS"/>      
+      <Head title="Portoflio NextJS" />
       <body className="antialiased">
         <Main />
         <NextScript />
